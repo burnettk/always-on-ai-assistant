3a3902644f1e168712e76bb087bc88e687cad04e
burnettk <burnettk@users.noreply.github.com>
2025-01-28 17:49:26 -0500

    nuther model

--- modules/gemini.py
+++ modules/gemini.py
@@ -32,6 +32,7 @@ safety_settings = [
 
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
+    # model_name="gemini-2.0-flash-exp",
     generation_config=generation_config,
     safety_settings=safety_settings,
 )

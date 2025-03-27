ac54a56f1debdde9f08bd1765c9ed3c1141f4562
burnettk <burnettk@users.noreply.github.com>
2025-03-27 19:26:15 -0400

    bin

---
+++ bin/no_scratch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+function error_handler() {
+  >&2 echo "Exited with BAD EXIT CODE '${2}' in ${0} script at line: ${1}."
+  exit "$2"
+}
+trap 'error_handler ${LINENO} $?' ERR
+set -o errtrace -o errexit -o nounset -o pipefail
+
+echo >scratchpad.md
+uv run python main_typer_assistant.py awaken --typer-file commands/template.py --scratchpad scratchpad.md --mode execute-no-scratch

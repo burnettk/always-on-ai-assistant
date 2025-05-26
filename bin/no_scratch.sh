f9cefe1ab62d440ce170a70f100443131324f908
burnettk <burnettk@users.noreply.github.com>
2025-05-26 18:34:34 -0400

    llm

--- bin/no_scratch.sh
+++ bin/no_scratch.sh
@@ -8,4 +8,4 @@ trap 'error_handler ${LINENO} $?' ERR
 set -o errtrace -o errexit -o nounset -o pipefail
 
 echo >scratchpad.md
-uv run python main_typer_assistant.py awaken --typer-file commands/template.py --scratchpad scratchpad.md --mode execute-no-scratch
+uv run python main_typer_assistant.py awaken --typer-file commands/template.py --scratchpad scratchpad.md --mode execute-no-scratch --llm deepseek
